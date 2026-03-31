f6000c15d87bad0539448668aeb480df1588bd6d
Elkin <elkin@atomicbot.ai>
2026-03-31 04:34:03 +0000

    Phase 1: Deep Body Grep, Developer Personas, and API Scrapes

---
+++ targets/farmerbutcher.com.au/historical_responses/10010244864827.php
@@ -0,0 +1,2 @@
+XML-RPC server accepts POST requests only.
+<!-- WP Optimize page cache - https://getwpo.com - page NOT cached -->
---
+++ targets/farmerbutcher.com.au/historical_responses/8453699141031.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+	<html lang="en-US">
+	<head>
+
+
+
+
+
+
+
+
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+	<title>Log In &lsaquo; Farmer Butcher &#8212; WordPress</title>
+	<meta name="robots" content="noindex, follow"/>
+<script type="text/javascript" id="woocommerce-google-analytics-integration-gtag-js-after">
+/* <![CDATA[ */
+/* Google Analytics for WooCommerce (gtag.js) */
+					window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					// Set up default consent state.
+					for ( const mode of [{"analytics_storage":"denied","ad_storage":"denied","ad_user_data":"denied","ad_personalization":"denied","region":["AT","BE","BG","HR","CY","CZ","DK","EE","FI","FR","DE","GR","HU","IS","IE","IT","LV","LI","LT","LU","MT","NL","NO","PL","PT","RO","SK","SI","ES","SE","GB","CH"]}] || [] ) {
+						gtag( "consent", "default", { "wait_for_update": 500, ...mode } );
+					}
+					gtag("js", new Date());
+					gtag("set", "developer_id.dOGY3NW", true);
+					gtag("config", "G-ZM2VCQG1EZ", {"track_404":true,"allow_google_signals":true,"logged_in":false,"linker":{"domains":[],"allow_incoming":false},"custom_map":{"dimension1":"logged_in"}});
+/* ]]> */
+</script>
+<script type="text/javascript" src="https://farmerbutcher.com.au/wp-includes/js/jquery/jquery.min.js?ver=3.7.1" id="jquery-core-js"></script>
+<script type="text/javascript" src="https://farmerbutcher.com.au/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.4.1" id="jquery-migrate-js"></script>
+<link rel="stylesheet" id="omnisend-woocommerce-checkout-block-checkout-newsletter-subscription-block-css" href="https://farmerbutcher.com.au/wp-content/plugins/omnisend-connect/includes/blocks/build/omnisend-checkout-block.css?ver=0.1.0" type="text/css" media="all"/>
+<link rel="stylesheet" id="omnisend-woocommerce-checkout-block-blocks-integration-css" href="https://farmerbutcher.com.au/wp-content/plugins/omnisend-connect/includes/blocks/build/omnisend-checkout-block.css?ver=0.1.0" type="text/css" media="all"/>
+<link rel="stylesheet" id="dashicons-css" href="https://farmerbutcher.com.au/wp-includes/css/dashicons.min.css?ver=6.7.1" type="text/css" media="all"/>
+<link rel="stylesheet" id="buttons-css" href="https://farmerbutcher.com.au/wp-includes/css/buttons.min.css?ver=6.7.1" type="text/css" media="all"/>
+<link rel="stylesheet" id="forms-css" href="https://farmerbutcher.com.au/wp-admin/css/forms.min.css?ver=6.7.1" type="text/css" media="all"/>
+<link rel="stylesheet" id="l10n-css" href="https://farmerbutcher.com.au/wp-admin/css/l10n.min.css?ver=6.7.1" type="text/css" media="all"/>
+<link rel="stylesheet" id="login-css" href="https://farmerbutcher.com.au/wp-admin/css/login.min.css?ver=6.7.1" type="text/css" media="all"/>
+	<meta name="referrer" content="strict-origin-when-cross-origin"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+		</head>
+	<body class="login no-js login-action-login wp-core-ui  locale-en-us">
+	<script type="text/javascript">
+/* <![CDATA[ */
+document.body.className = document.body.className.replace('no-js','js');
+/* ]]> */
+</script>
+
+				<h1 class="screen-reader-text">Log In</h1>
+			<div id="login">
+		<h1 role="presentation" class="wp-login-logo"><a href="https://wordpress.org/">Powered by WordPress</a></h1>
+	
+		<form name="loginform" id="loginform" action="https://farmerbutcher.com.au/wp-login.php" method="post">
+			<p>
+				<label for="user_login">Username or Email Address</label>
+				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" autocomplete="username" required="required"/>
+			</p>
+
+			<div class="user-pass-wrap">
+				<label for="user_pass">Password</label>
+				<div class="wp-pwd">
+					<input type="password" name="pwd" id="user_pass" class="input password-input" value="" size="20" autocomplete="current-password" spellcheck="false" required="required"/>
+					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Show password">
+						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"/> <label for="rememberme">Remember Me</label></p>
+			<p class="submit">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In"/>
+									<input type="hidden" name="redirect_to" value="https://farmerbutcher.com.au/wp-admin/"/>
+									<input type="hidden" name="testcookie" value="1"/>
+			</p>
+		</form>
+
+					<p id="nav">
+				<a rel="nofollow" class="wp-login-register" href="https://farmerbutcher.com.au/wp-login.php?action=register">Register</a> | <a class="wp-login-lost-password" href="https://farmerbutcher.com.au/wp-login.php?action=lostpassword">Lost your password?</a>			</p>
+			<script type="text/javascript">
+/* <![CDATA[ */
+function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
+wp_attempt_focus();
+if ( typeof wpOnload === 'function' ) { wpOnload() }
+/* ]]> */
+</script>
+		<p id="backtoblog">
+			<a href="https://farmerbutcher.com.au/">&larr; Go to Farmer Butcher</a>		</p>
+			</div>
+		
+	<script type="text/javascript" src="https://farmerbutcher.com.au/wp-includes/js/clipboard.min.js?ver=2.0.11" id="clipboard-js"></script>
+<script type="text/javascript" id="zxcvbn-async-js-extra">
+/* <![CDATA[ */
+var _zxcvbnSettings = {"src":"https:\/\/farmerbutcher.com.au\/wp-includes\/js\/zxcvbn.min.js"};
+/* ]]> */
+</script>
+<script type="text/javascript" src="https://farmerbutcher.com.au/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script type="text/javascript" src="https://farmerbutcher.com.au/wp-includes/js/dist/hooks.min.js?ver=4d63a3d491d11ffd8ac6" id="wp-hooks-js"></script>
+<script type="text/javascript" src="https://farmerbutcher.com.au/wp-includes/js/dist/i18n.min.js?ver=5e580eb46a90c2b997e6" id="wp-i18n-js"></script>
+<script type="text/javascript" id="wp-i18n-js-after">
+/* <![CDATA[ */
+wp.i18n.setLocaleData( { 'text direction\u0004ltr': [ 'ltr' ] } );
+/* ]]> */
+</script>
+<script type="text/javascript" id="password-strength-meter-js-extra">
+/* <![CDATA[ */
+var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"Weak","good":"Medium","strong":"Strong","mismatch":"Mismatch"};
+/* ]]> */
+</script>
+<script type="text/javascript" src="https://farmerbutcher.com.au/wp-admin/js/password-strength-meter.min.js?ver=6.7.1" id="password-strength-meter-js"></script>
+<script type="text/javascript" src="https://farmerbutcher.com.au/wp-includes/js/underscore.min.js?ver=1.13.7" id="underscore-js"></script>
+<script type="text/javascript" id="wp-util-js-extra">
+/* <![CDATA[ */
+var _wpUtilSettings = {"ajax":{"url":"\/wp-admin\/admin-ajax.php"}};
+/* ]]> */
+</script>
+<script type="text/javascript" src="https://farmerbutcher.com.au/wp-includes/js/wp-util.min.js?ver=6.7.1" id="wp-util-js"></script>
+<script type="text/javascript" src="https://farmerbutcher.com.au/wp-includes/js/dist/dom-ready.min.js?ver=f77871ff7694fffea381" id="wp-dom-ready-js"></script>
+<script type="text/javascript" src="https://farmerbutcher.com.au/wp-includes/js/dist/a11y.min.js?ver=3156534cc54473497e14" id="wp-a11y-js"></script>
+<script type="text/javascript" id="user-profile-js-extra">
+/* <![CDATA[ */
+var userProfileL10n = {"user_id":"0","nonce":"dd8b530ae0"};
+/* ]]> */
+</script>
+<script type="text/javascript" src="https://farmerbutcher.com.au/wp-admin/js/user-profile.min.js?ver=6.7.1" id="user-profile-js"></script>
+	</body>
+	</html>
+	
+<!-- Cached by WP-Optimize - https://getwpo.com - Last modified: January 17, 2025 1:09 pm (Australia/Sydney UTC:11) -->
+
